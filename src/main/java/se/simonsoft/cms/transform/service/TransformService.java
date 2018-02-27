3eb4de85c24cb5b271f1f7d5c7e1456002c9c97f
Jonathan Andersson <jonathan.andersson@pdsvision.se>
2018-02-27 10:51:53 +0100

    Interface starts with transform method void. Should it commit within the same operation? then it can be void. Or should it have method for commiting result?

--- src/main/java/se/simonsoft/cms/transform/service/TransformService.java
+++ src/main/java/se/simonsoft/cms/transform/service/TransformService.java
@@ -1,5 +1,9 @@
 package se.simonsoft.cms.transform.service;
 
-public interface TransformService {
+import se.simonsoft.cms.item.CmsItem;
+import se.simonsoft.cms.transform.config.databind.TransformConfig;
 
+public interface TransformService {
+	
+	void transform(CmsItem item, TransformConfig config);
 }
