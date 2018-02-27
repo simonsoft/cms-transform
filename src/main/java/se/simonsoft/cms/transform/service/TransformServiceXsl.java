171e9c13f242eed20efc3bfb5295da0505095111
Jonathan Andersson <jonathan.andersson@pdsvision.se>
2018-02-27 11:54:24 +0100

    Itemlookup is per repo, injected per repo map. Chnaged to TransformerServiceFactory to be able to create instances with given stylesheet from config.

--- src/main/java/se/simonsoft/cms/transform/service/TransformServiceXsl.java
+++ src/main/java/se/simonsoft/cms/transform/service/TransformServiceXsl.java
@@ -19,30 +19,50 @@ import se.simonsoft.cms.item.commit.CmsCommit;
 import se.simonsoft.cms.item.impl.CmsItemIdArg;
 import se.simonsoft.cms.item.info.CmsItemLookup;
 import se.simonsoft.cms.transform.config.databind.TransformConfig;
-import se.simonsoft.cms.xmlsource.handler.s9api.XmlSourceDocumentS9api;
+import se.simonsoft.cms.xmlsource.transform.TransformOptions;
 import se.simonsoft.cms.xmlsource.transform.TransformerService;
+import se.simonsoft.cms.xmlsource.transform.TransformerServiceFactory;
 
 public class TransformServiceXsl implements TransformService {
 	
 	private final CmsCommit commit;
-	private final CmsItemLookup lookup;
-	private final TransformerService transformerService;
+	private final Map<CmsRepository,CmsItemLookup> itemLookups;
+	private final TransformerServiceFactory transformerServiceFactory;
+	private final Map<String, Source> stylesheets;
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(TransformServiceXsl.class);
 
 	@Inject
 	public TransformServiceXsl(
 			CmsCommit commit,
-			CmsItemLookup itemLookup,
-			TransformerService transfromerService
+			Map<CmsRepository,CmsItemLookup> itemLookup,
+			TransformerServiceFactory transfromerServiceFactory,
+			Map<String, Source> stylesheets
 			) {
 		
 		this.commit = commit;
-		this.lookup = itemLookup;
-		this.transformerService = transfromerService;
+		this.itemLookups = itemLookup;
+		this.transformerServiceFactory = transfromerServiceFactory;
+		this.stylesheets = stylesheets;
 	}
 
 	@Override
 	public void transform(CmsItem item, TransformConfig config) {
 		
+		if (!config.getOptions().getType().equals("xsl")) {
+			throw new IllegalArgumentException("TransformServiceXsl can only handle xsl transforms but was given: " + config.getOptions().getType());
+		}
+		
+		final CmsItemId itemId = item.getId();
+		final CmsItemLookup lookup = itemLookups.get(itemId.getRepository());
+		final Source stylesheetPath = getStylesheetSource(itemId, config);
+		
+		final TransformerService transformerService = transformerServiceFactory.buildTransformerService(stylesheetPath);
+		transformerService.setItemLookup(lookup);
+		
+	}
+	
 	private Source getStylesheetSource(CmsItemId itemId, TransformConfig config) {
 		final String stylesheetPath = config.getOptions().getParams().get("stylesheet");
 		Source resultSource;
