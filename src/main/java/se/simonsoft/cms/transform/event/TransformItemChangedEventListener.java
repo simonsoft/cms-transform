838f27528ce1ff829c0f438ee60ba941301f7626
Jonathan Andersson <jonathan.andersson@pdsvision.se>
2018-02-27 09:32:03 +0100

    Lookup is at least needed for the TransformerService.

--- src/main/java/se/simonsoft/cms/transform/event/TransformItemChangedEventListener.java
+++ src/main/java/se/simonsoft/cms/transform/event/TransformItemChangedEventListener.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import se.simonsoft.cms.item.CmsItem;
 import se.simonsoft.cms.item.commit.CmsCommit;
 import se.simonsoft.cms.item.events.ItemChangedEventListener;
+import se.simonsoft.cms.item.info.CmsItemLookup;
 import se.simonsoft.cms.transform.config.TransformConfiguration;
 import se.simonsoft.cms.xmlsource.transform.TransformerService;
 
@@ -13,17 +14,20 @@ public class TransformItemChangedEventListener implements ItemChangedEventListen
 	private final TransformConfiguration transformConfiguration;
 	private final TransformerService transformerService;
 	private final CmsCommit commit;
+	private final CmsItemLookup lookup;
 
 	@Inject
 	public TransformItemChangedEventListener(
 			TransformConfiguration transformConfiguration,
 			TransformerService transformerService,
-			CmsCommit commit
+			CmsCommit commit,
+			CmsItemLookup lookup
 			) {
 		
 		this.transformConfiguration = transformConfiguration;
 		this.transformerService = transformerService;
 		this.commit = commit;
+		this.lookup = lookup;
 	
 	}
 
