1f0dd951d2556598f54fc831d0fd556d94f37984
Jonathan Andersson <jonathan.andersson@pdsvision.se>
2018-02-27 10:49:26 +0100

    The responsiblity of commiting and transforming will be done in transformService, no need for lookup or CmsCommit

--- src/main/java/se/simonsoft/cms/transform/event/TransformItemChangedEventListener.java
+++ src/main/java/se/simonsoft/cms/transform/event/TransformItemChangedEventListener.java
@@ -3,9 +3,7 @@ package se.simonsoft.cms.transform.event;
 import javax.inject.Inject;
 
 import se.simonsoft.cms.item.CmsItem;
-import se.simonsoft.cms.item.commit.CmsCommit;
 import se.simonsoft.cms.item.events.ItemChangedEventListener;
-import se.simonsoft.cms.item.info.CmsItemLookup;
 import se.simonsoft.cms.transform.config.TransformConfiguration;
 import se.simonsoft.cms.xmlsource.transform.TransformerService;
 
@@ -13,21 +11,15 @@ public class TransformItemChangedEventListener implements ItemChangedEventListen
 	
 	private final TransformConfiguration transformConfiguration;
 	private final TransformerService transformerService;
-	private final CmsCommit commit;
-	private final CmsItemLookup lookup;
 
 	@Inject
 	public TransformItemChangedEventListener(
 			TransformConfiguration transformConfiguration,
-			TransformerService transformerService,
-			CmsCommit commit,
-			CmsItemLookup lookup
+			TransformerService transformerService
 			) {
 		
 		this.transformConfiguration = transformConfiguration;
 		this.transformerService = transformerService;
-		this.commit = commit;
-		this.lookup = lookup;
 	
 	}
 
