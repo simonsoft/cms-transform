526b1f3ddabceb3244ec90a3630f1445301318f2
Thomas Åkesson <thomas.akesson@simonsoft.se>
2021-02-02 10:09:24 +0100

    Avoid duplicate binding of identity.xsl.

--- src/test/java/se/simonsoft/cms/transform/testconfig/TestTransformModule.java
+++ src/test/java/se/simonsoft/cms/transform/testconfig/TestTransformModule.java
@@ -29,7 +29,7 @@ public class TestTransformModule extends AbstractModule{
 	@Override
 	protected void configure() {
 		MapBinder<String, Source> sourceBinder = MapBinder.newMapBinder(binder(), String.class, Source.class);
-		sourceBinder.addBinding("identity.xsl").toInstance(new StreamSource(this.getClass().getClassLoader().getResourceAsStream("se/simonsoft/cms/xmlsource/transform/identity.xsl")));
+		//sourceBinder.addBinding("identity.xsl").toInstance(new StreamSource(this.getClass().getClassLoader().getResourceAsStream("se/simonsoft/cms/xmlsource/transform/identity.xsl")));
 		// Just for cms-transform testing.
 		sourceBinder.addBinding("transform-multiple-output.xsl").toInstance(new StreamSource(this.getClass().getClassLoader().getResourceAsStream("se/simonsoft/cms/transform/datasets/repo1/stylesheet/transform-multiple-output.xsl")));
 		sourceBinder.addBinding("transform-single-output.xsl").toInstance(new StreamSource(this.getClass().getClassLoader().getResourceAsStream("se/simonsoft/cms/transform/datasets/repo1/stylesheet/transform-single-output.xsl")));
